7924e3c4974fc50198a249a986bb7a083f1b642a
Ivan Kornasevich <kornasevich@gmail.com>
2023-12-20 07:15:41 +0100

    add Apply type

--- src/main/scala/com/meineliebe/lox/typelevel/Core.scala
+++ src/main/scala/com/meineliebe/lox/typelevel/Core.scala
@@ -18,13 +18,42 @@ type _9 = Succ[_8]
 trait Dec[N <: Nat] {
   type R <: Nat
 }
-
+type _Dec[N <: Nat, O <: Nat] = Dec[N] { type R = O }
 object Dec {
-  private type _Dec[N <: Nat, O <: Nat] = Dec[N] { type R = O }
   given basic: _Dec[Succ[_0], _0] = new Dec[Succ[_0]] { type R = _0 }
   given inductive[N <: Nat](using d: Dec[N]): _Dec[Succ[N], Succ[d.R]] = new Dec[Succ[N]] { type R = Succ[d.R] }
   def test[N <: Nat, O <: Nat](using dec: _Dec[N, O]): _Dec[N, O] = dec
-  def apply[N <: Nat](using dec: Dec[N]): Dec[N] = dec
+  def apply[N <: Nat](using dec: Dec[N]): _Dec[N, dec.R] = dec
+}
+
+//trait Apply[F[_ <: Nat, _ <: Nat], A <: Nat] {
+//  type R <: Nat
+//}
+//
+//type _Apply[F[_ <: Nat, _ <: Nat], A <: Nat, O <: Nat] = Apply[F, A] {type R = O}
+//
+//object Apply {
+//  def test[F[_ <: Nat, _ <: Nat], A <: Nat, O <: Nat](using f: F[A, O]): _Apply[F, A, O] = new Apply[F, A] {
+//    type R = O
+//  }
+//}
+
+trait Apply[F[_ <: Nat, _ <: Nat], A <: Nat] {
+  type R <: Nat
+}
+
+type _Apply[F[_ <: Nat, _ <: Nat], A <: Nat, O <: Nat] = Apply[F, A] { type R = O }
+
+object Apply {
+  given basic[F[_ <: Nat, _ <: Nat], A <: Nat, O <: Nat](using F[A, O]): _Apply[F, A, O] = new Apply[F, A] {
+    type R = O
+  }
+
+  def test[F[_ <: Nat, _ <: Nat], A <: Nat, O <: Nat](using ap: Apply[F, A]): _Apply[F, A, ap.R] = new Apply[F, A] {
+    type R = ap.R
+  }
+
+  def apply[F[_ <: Nat, _ <: Nat], A <: Nat](using ap: Apply[F, A]): _Apply[F, A, ap.R] = ap
 }
 
 trait ==[A <: Nat, B <: Nat]
--- src/test/scala/com/meineliebe/lox/typelevel/CoreTest.scala
+++ src/test/scala/com/meineliebe/lox/typelevel/CoreTest.scala
@@ -1,12 +1,17 @@
 package com.meineliebe.lox.typelevel
 
 import com.meineliebe.lox.typelevel.{< as lt, <= as ltq, == as teq}
+import com.meineliebe.lox.typelevel.Dec.given
 import org.scalatest.matchers.should
 import org.scalatest.wordspec.AnyWordSpec
+import org.tpolecat.typename.TypeName
 
 import scala.runtime.stdLibPatches.Predef.summon
 
 class CoreTest extends AnyWordSpec with should.Matchers {
+
+  def printType[A](value: A)(using typename: TypeName[A]): Unit = println(typename.value.replace("com.meineliebe.lox.typelevel.hlist", "").replace("com.meineliebe.lox.typelevel.", ""))
+
 
   "Dec" in {
     Dec.test[_9, _8]
@@ -27,5 +32,11 @@ class CoreTest extends AnyWordSpec with should.Matchers {
     ltq[_0, _0]
     ltq[_1, _9]
     ltq[_9, _9]
+  }
+
+  "Apply" in {
+    val x = Apply[_Dec, _1]
+    val y = Dec[_1]
+    teq[x.R, y.R]
   }
 }
